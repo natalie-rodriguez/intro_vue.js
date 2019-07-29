48daea84af265837258ddbf8ad1372f5cbf7931c
natalie <nataliediazrodriguez@gmail.com>
2019-07-29 19:42:40 -0400

    new commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Goodbye Vue!",
+    name: "Natalie"
+  },
+  methods: {
+    changeMessage: function() {
+      console.log('i am in the change message function');
+      his.message = "Goobye";t
+
+      
+    }
+  }
+});
